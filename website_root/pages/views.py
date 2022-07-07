1eec691e547713170ef4f3eb256d476cf8b821ac
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-07-07 10:29:56 +0300

    Add view for Contact page

--- website_root/pages/views.py
+++ website_root/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Page
+from .forms import ContactForm
 
 def render_pages(request, pagename):
     permalink = "".join(["/", pagename])
@@ -10,3 +11,11 @@ def render_pages(request, pagename):
         "page_list": Page.objects.all()
     }
     return render(request, 'pages/page.html', context)
+
+def render_contact_form(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return render(request, 'contact/form_sent.html', {'page_list': Page.objects.all()})
+    form = ContactForm()
+    return render(request, 'contact/contact.html', {'form': form, 'page_list': Page.objects.all()})
