891edb68e50f40e8c5a899f741c11af99eff6dac
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-07-07 10:30:36 +0300

    Add url for Contact page view

--- website_root/pages/urls.py
+++ website_root/pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.render_pages, {'pagename': 'home'}),
+    path('contact', views.render_contact_form, name='contact'),
     path('<str:pagename>', views.render_pages, name='home'),
 ]
