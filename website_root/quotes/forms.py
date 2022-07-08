fb6244d6ef45f2b4b4c5855419d05d5a4bcfcead
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-07-08 17:59:00 +0300

    Show asterisks after labels of the required fields

--- website_root/quotes/forms.py
+++ website_root/quotes/forms.py
@@ -2,6 +2,7 @@ from django.forms import ModelForm
 from .models import Quote
 
 class QuoteForm(ModelForm):
+    required_css_class = 'required'
     class Meta:
         model = Quote
         fields = [
