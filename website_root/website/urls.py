926f770a16acf27e0dda58b7d264ee914a4818f3
Danil Tsygolnik <tsygolnikd@gmail.com>
2022-07-09 16:21:06 +0300

    Add url for the page after successful registration

--- website_root/website/urls.py
+++ website_root/website/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from quotes.views import QuoteList, QuoteView
 from registration.views import Register
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/success/',TemplateView.as_view(template_name="registration/success.html"), name ='register-success'),
     path('register/', Register.as_view(), name='register'),
     path('quote/', include('quotes.urls')),
     path('show/quote/<int:pk>', QuoteView.as_view(), name='quote-details'),
